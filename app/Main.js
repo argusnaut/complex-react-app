e5bd76cee794a003330533b4e2478ef9a6e40d05
Vitor Argus <vitor.argus@outlook.com>
2022-08-05 22:25:28 -0300

    Initial commit

---
+++ app/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Custom Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+
+      <HomeGuest />
+
+      <Footer />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
